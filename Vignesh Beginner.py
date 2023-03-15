7e39e9540da895d6e1e0d7af7da8a21e34ec0fe6
Elena Shu Xian Foong <73242679+elenasxfoong@users.noreply.github.com>
2023-03-15 15:08:28 +0800

    Create Vignesh Beginner.py

---
+++ Vignesh Beginner.py
@@ -0,0 +1,17 @@
+import random
+
+track = [["Daydreaming by Harry Styles"],
+         [ "Workin Out by JID"],
+         ["Old Love by yuji"],
+        ["Eventually by Tame Impala"],
+         ["After Hours by The Weekend"],
+         ["Ghostin by Ariana Grande"],
+          ["Hey, Mickey! by Baby Tate"],
+         ["Low by SZA"],
+         ["Pink + White by Frank Ocean"],
+         ["Here With Me by d4vd"]]
+
+for i in range (1,4);
+  shuffle = random.randint(1,10)
+  print (shuffle)
+  print (track[shuffle])
